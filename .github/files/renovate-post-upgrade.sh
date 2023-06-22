469a7106e47b334c310c19b6761f5fd563cb2166
Brad Jorsch <brad.jorsch@automattic.com>
2023-06-22 16:52:48 -0400

    Debug?

--- .github/files/renovate-post-upgrade.sh
+++ .github/files/renovate-post-upgrade.sh
@@ -33,9 +33,9 @@ composer config --global cache-dir /tmp/renovate/cache/others/composer
 
 # Do the pnpm and changelogger installs.
 cd "$BASE"
-pnpm --quiet install
+pnpm install
 cd projects/packages/changelogger
-composer --quiet update
+composer update
 cd "$BASE"
 
 # Add change files for anything that changed. But ignore .npmrc, renovate mangles those.
